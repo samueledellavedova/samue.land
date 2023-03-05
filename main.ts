750bb492be682061b50c5eafc00745ff253120f4
Samuele Della Vedova <46026355+samueledellavedova@users.noreply.github.com>
2023-03-05 16:41:32 +0100

    feat: add basic router

--- main.ts
+++ main.ts
@@ -2,15 +2,34 @@ const decoder = new TextDecoder("utf-8");
 const listener = Deno.listenTls({
   port: 443,
   cert: decoder.decode(Deno.readFileSync("cert.pem")),
-  key: decoder.decode(Deno.readFileSync("key.pem"))
+  key: decoder.decode(Deno.readFileSync("key.pem")),
 });
 
 console.info("listening");
 
 for await (const conn of listener) {
   (async () => {
-    for await (const req of Deno.serveHttp(conn)) {
-      req.respondWith(new Response("hi"));
+    for await (const evt of Deno.serveHttp(conn)) {
+      evt
+        .respondWith(handleRequest(evt.request))
+        .catch(() => void 0);
     }
   })();
+}
+
+function handleRequest(req: Request) {
+  const url = new URL(req.url);
+
+  switch (url.pathname) {
+    case "/":
+      if (req.method !== "GET")
+        return new Response("", {
+          status: 405,
+        });
+      return new Response("hi");
+    default:
+      return new Response("nope", {
+        status: 404,
+      });
+  }
 }
